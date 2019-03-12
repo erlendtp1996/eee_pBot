fb200cc387db21ba6bdd84dbb59a5b240ef2315b
Thomas Erlendson <erlendtp1996@gmail.com>
2019-03-11 21:34:30 -0400

    update

--- eeeP0Bot/__init__.py
+++ eeeP0Bot/__init__.py
@@ -4,8 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 app.run(environ.get('PORT'))
 
-app.run(host= '0.0.0.0', port=environ.get('PORT'))
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', port=environ.get('PORT', 5000))
