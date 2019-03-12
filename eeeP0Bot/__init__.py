6b8d0514744b4fed6e64a21f64265442f7fd35e7
Thomas Erlendson <erlendtp1996@gmail.com>
2019-03-11 21:23:21 -0400

    update

---
+++ eeeP0Bot/__init__.py
@@ -0,0 +1,11 @@
+from os import environ
+from flask import Flask
+
+app = Flask(__name__)
+app.run(environ.get('PORT'))
+
+app.run(host= '0.0.0.0', port=environ.get('PORT'))
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
