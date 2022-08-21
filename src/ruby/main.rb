edf180ed45efff26b6743a18c127cba0a83c456f
Thomas Erlendson <thomaserlendson@Thomass-MacBook-Pro.local>
2022-08-21 13:34:14 -0400

    Put auth methods into module

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -1,19 +1,68 @@
 require 'uri'
 require 'base64'
 require 'net/http'
-require 'cgi'
 require 'openssl'
 require 'json'
 
-def setEnv (filePath)
-    file = File.open(filePath)
-    fileData = file.readlines.map(&:chomp)
-    file.close
-    fileData.each do |data|
-        var = data.split("=")
-        ENV[var[0]] = var[1]
+module OAuthAuthentication
+    extend self
+    def createOAuthHeader (method, baseURL, options = {})
+        consumerSecret = options[:consumer_secret]
+        accessTokenSecret = options[:access_token_secret]
+        options.delete( :consumer_secret )
+        options.delete( :access_token_secret )
+
+        parameterString = createParameterString( options )
+        baseString = createBaseString( method, baseURL, parameterString)
+        signingKey = createSigningKey( consumerSecret, accessTokenSecret)
+        signature = createSignature( signingKey, baseString )
+
+        authString = createAuthString( options, signature )
+    end
+
+    private
+
+    def encodeString ( str )
+        if !str.instance_of? String
+            str = str.to_s
+        end
+        str = URI.escape( str )
+        return str.gsub("+", "%2B").gsub(",", "%2C").gsub("!", "%21").gsub(":", "%3A").gsub("/", "%2F").gsub("=", "%3D").gsub("&", "%26")
+    end
+
+    def createParameterString ( parameters )
+        parameterString = ""
+        sortedKeys = parameters.keys.sort
+        sortedKeys.each_with_index do |key, index|
+            parameterString << encodeString( key ) << "=" << encodeString( parameters[key] ) << (index == sortedKeys.length - 1 ? "" : "&")
+        end
+        return parameterString
+    end
+    
+    def createBaseString (method, baseURL, parameterString)
+        return method.upcase + "&" + encodeString( baseURL ) + "&" + encodeString( parameterString )
+    end
+    
+    def createSigningKey ( consumerSecret, accessTokenSecret ) 
+        return encodeString( consumerSecret ) + "&" + encodeString( accessTokenSecret )
+    end 
+    
+    def createSignature ( signingKey, baseString )
+        return Base64.encode64("#{OpenSSL::HMAC.digest('sha1', signingKey, baseString)}").strip.gsub("+", "%2B").gsub("=", "%3D")
+    end
+    
+    def createAuthString( parameters, signature )
+        authString = "OAuth "
+        parameters.delete(:status)
+        parameters = parameters.merge({ oauth_signature: signature })
+        sortedKeys = parameters.keys.sort
+        sortedKeys.each_with_index do |key, index|
+            authString << key.to_s << "=" << '"' << parameters[key] << '"' << (index == sortedKeys.length - 1 ? "" : ',')
+        end
+        return authString
     end
 end
+
 
 def updateStatus (status)
     method = "POST"
@@ -28,72 +77,43 @@ def updateStatus (status)
         oauth_signature_method: "HMAC-SHA1",
         oauth_timestamp: timestamp, 
         oauth_token: ENV["ACCESS_TOKEN"],
-        oauth_version: "1.0"
+        oauth_version: "1.0",
+        consumer_secret: ENV["CONSUMER_SECRET"],
+        access_token_secret: ENV["ACCESS_TOKEN_SECRET"]
     }
 
-    parameterString = createParameterString(params)
-    baseString = createBaseString( method, baseURL, parameterString)
-    signingKey = createSigningKey( ENV["CONSUMER_SECRET"], ENV["ACCESS_TOKEN_SECRET"])
-    signature = createSignature( signingKey, baseString )
-
-    authString = createAuthString( params, signature )
-    sendRequest(baseURL, status, authString)
+    authHeader = OAuthAuthentication.createOAuthHeader( method, baseURL, params )
+    baseURL = baseURL + "?status=#{status}"
+    sendPostRequest( baseURL , { Authorization: authHeader } )
 end
 
-def sendRequest (baseURL, status, authString)
-
-    url = URI (baseURL + "?status=#{status}")
+def sendPostRequest (uri, requestOptions = {})
+    url = URI (uri)
     http = Net::HTTP.new(url.host, url.port)
 
     request = Net::HTTP::Post.new(url)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request['Authorization'] = authString
+    
+    requestOptions.keys.each do |key|
+        request[key] = requestOptions[key]
+    end
+
+    puts request["Authorization"]
+
     response = http.request(request)
-    puts response.to_s
+    response.code
+    puts response
 end
 
-def encodeString (str)
-    if !str.instance_of? String
-        str = str.to_s
+def setEnv (filePath)
+    file = File.open(filePath)
+    fileData = file.readlines.map(&:chomp)
+    file.close
+    fileData.each do |data|
+        var = data.split("=")
+        ENV[var[0]] = var[1]
     end
-    str = URI.escape(str)
-    return str.gsub("+", "%2B").gsub(",", "%2C").gsub("!", "%21").gsub(":", "%3A").gsub("/", "%2F").gsub("=", "%3D").gsub("&", "%26")
-end
-
-def createParameterString (parameters)
-    parameterString = ""
-    sortedKeys = parameters.keys.sort
-    sortedKeys.each_with_index do |key, index|
-        parameterString << encodeString(key) << "=" << encodeString(parameters[key]) << (index == sortedKeys.length - 1 ? "" : "&")
-    end
-    return parameterString
-end
-
-def createBaseString (method, baseURL, parameterString)
-    return method.upcase + "&" + encodeString(baseURL) + "&" + encodeString(parameterString)
-end
-
-def createSigningKey (consumerSecret, oauthSecret) 
-    encodeString(consumerSecret) + "&" + encodeString(oauthSecret)
-end 
-
-def createSignature (key, signature)
-    Base64.encode64("#{OpenSSL::HMAC.digest('sha1', key, signature)}").strip.gsub("+", "%2B").gsub("=", "%3D")
-end
-
-def createAuthString(parameter, signature)
-    authString = "OAuth "
-    parameter.delete(:status)
-    parameter = parameter.merge({ oauth_signature: signature })
-
-    sortedKeys = parameter.keys.sort
-
-    sortedKeys.each_with_index do |key, index|
-        authString << key.to_s << "=" << '"' << parameter[key] << '"' << (index == sortedKeys.length - 1 ? "" : ',')
-    end
-
-    return authString
 end
 
 setEnv("../../.env")
